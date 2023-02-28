c5b7c78e96c902010b40b07c02cd97619ce39a52
Maxim <mlyulenko@gmail.com>
2023-02-28 16:15:09 +0300

    Unit convertor created.

---
+++ unitconvertor.py
@@ -0,0 +1,46 @@
+def inches_to_centimeters(x: [int, float]) -> float:
+    return x * 2.54
+
+
+def centimeters_to_inches(x: [int, float]) -> float:
+    return x / 2.54
+
+
+def miles_to_kilometers(x: [int, float]) -> float:
+    return x * 1.609
+
+
+def kilometers_to_miles(x: [int, float]) -> float:
+    return x / 1.609
+
+
+def pounds_to_kilograms(x: [int, float]) -> float:
+    return x / 2.205
+
+
+def kilograms_to_pounds(x: [int, float]) -> float:
+    return x * 2.205
+
+
+def grams_to_ounces(x: [int, float]) -> float:
+    return x / 28.35
+
+
+def ounces_to_grams(x: [int, float]) -> float:
+    return x * 28.35
+
+
+def gallons_to_liters(x: [int, float]) -> float:
+    return x * 4.546
+
+
+def liters_to_gallons(x: [int, float]) -> float:
+    return x / 4.546
+
+
+def pints_to_liters(x: [int, float]) -> float:
+    return x / 1.76
+
+
+def liters_to_pints(x: [int, float]) -> float:
+    return x * 1.76
